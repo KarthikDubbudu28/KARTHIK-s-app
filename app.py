baa103adc7e357662d24a990683e8418f6311c7d
KarthikDubbudu28 <karthikdubbudu28@gmail.com>
2025-05-09 14:26:30 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,15 @@
 import streamlit as st
 import pandas as pd
 
-st.title('Welcome to the Temperature Prediction App')
+st.title('Welcome to the Temperature Prediction App🌡️')
+@st.cache_data
+def load_data():
+    df = pd.read_csv("beijing_cleaned.csv")
+    return df
 
+# Call the load function
+data = load_data()
+
+# Display the dataset
+st.write("### Here's the cleaned Beijing dataset:")
+st.dataframe(data)
