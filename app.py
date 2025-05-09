3137b22b5a764877f882f807a871d60d5f0e6533
KarthikDubbudu28 <karthikdubbudu28@gmail.com>
2025-05-09 22:11:34 +0100

    Update app.py

--- app.py
+++ app.py
@@ -5,10 +5,7 @@ st.set_page_config(page_title="Temperature Prediction App", layout="wide")
 st.title("🌡️ Temperature Prediction App")
 st.markdown("Welcome to the Temperature Prediction App for Beijing City!")
 
-# Sidebar Navigation
-with st.sidebar:
-    st.header("📂 Pages")
-    page = st.radio("Navigate to:", ["Dataset Overview", "Explore EDA", "Model Prediction"])
+
 
 
 
